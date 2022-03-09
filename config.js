34d40b25d2483bd95f53ff1bf22039d264700715
asedekmtl <kmtdlah@gmail.com>
2022-03-09 18:37:00 +0700

    asedekmtl

---
+++ config.js
@@ -0,0 +1,62 @@
+global.DeveloperMode = 'false' //true Or false
+global.linkGC = ['https://chat.whatsapp.com/FYm1X4Bg19G5TatkN1y8hK']
+global.Owner = {
+  // use the phone number with the country code, for example Indonesia '62' America '1' etc
+  '6285719116912': { // put your number here
+    name: 'Owner Ohh',
+    isDev: true, // if true this number will send if bot have bug and developer mode true
+    isReport: true, // if true this number will send if user use command /report or /join
+    isCreator: true // if true this number will send if user use command /owner or /creator
+  },
+  '6285719116912': { // put your number here
+    name: 'Dave',
+    isDev: false, // if true this number will send if bot have bug and developer mode true
+    isReport: true, // if true this number will send if user use command /report or /join
+    isCreator: false // if true this number will send if user use command /owner or /creator
+  },
+  '6285719116912': {
+    name: 'Adiixyz',
+    isReport: true, // if true this number will send if user use command /report or /join
+  },
+  '6285719116912': {
+    isReport: true
+  },
+  '6285719116912': {} // ignore if you don't want anything, like name dll
+}
+global.mods = ['6285719116912'] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais'
+}
+
+// Sticker WM
+global.packname = 'wa.me/6285719116912'
+global.author = 'Ohh'
+
+global.multiplier = 39 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
